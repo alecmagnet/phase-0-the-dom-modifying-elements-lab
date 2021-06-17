49a59429739db6a4ecfd6fde8767a851792160ea
Alec Magnet <amagnet@gmail.com>
2021-06-17 19:49:44 -0400

    Assignment completed

--- index.js
+++ index.js
@@ -1,1 +1,6 @@
 // Write your code here!
+document.body.removeChild(main);
+const newHeader = document.createElement('h1');
+document.body.appendChild(newHeader);
+newHeader.setAttribute('id', "victory");
+newHeader.textContent = "Alec is the champion";
